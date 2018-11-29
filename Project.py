c12d319dfbe22217643503a02a46419dabd8db8d
Andre Dvorak <advorak98@gmail.com>
2018-11-29 12:48:55 -0500

    Added tokenization

--- Project.py
+++ Project.py
@@ -1,4 +1,18 @@
+#Takes string input and returns array with each index another sentence
+def tokenize(f):
+    a = [] #Stores tokenized sentences
+    start = 0 #Stores starting location
+    current = 0 #Stores current location
+    for q in f:
+        if(q=='.'):
+            a.append(f[start:current])
+            start = current+2
+        current += 1
+    return a
+
+#Open text file
 with open(input("Enter a filename: "), 'r') as f:
     file = f.read()
-print(file)
 
+#Tokenize input into sentences
+array = tokenize(file)
