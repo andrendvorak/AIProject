b95e62bcc69740992183b05efc0d8143425402fe
Andre Dvorak <advorak98@gmail.com>
2018-11-29 13:15:06 -0500

    Added tokenization

--- Project.py
+++ Project.py
@@ -1,10 +1,12 @@
+import nltk
+
 #Takes string input and returns array with each index another sentence
 def tokenize(f):
     a = [] #Stores tokenized sentences
     start = 0 #Stores starting location
     current = 0 #Stores current location
     for q in f:
-        if(q=='.'):
+        if(q=='.' or q=='!' or q=='?'):
             a.append(f[start:current])
             start = current+2
         current += 1
@@ -15,4 +17,9 @@ with open(input("Enter a filename: "), 'r') as f:
     file = f.read()
 
 #Tokenize input into sentences
-array = tokenize(file)
+tokenizedInput = tokenize(file)
+print("Finished tokenization")
+
+
+
+#print("Finished cleaning")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AIProject
+
+Using Python 3.6 and NLTK 3.4
