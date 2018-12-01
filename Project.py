be0ea63e2d5f38bfe4a76b96ca7e1e612e8e993d
Andre Dvorak <advorak98@gmail.com>
2018-12-01 14:17:06 -0500

    Beginning of k-means implementation

--- Project.py
+++ Project.py
@@ -1,4 +1,5 @@
 import nltk
+from random import randint
 
 #Takes string input and returns array with each index another sentence
 def tokenize(f):
@@ -33,17 +34,49 @@ def counting(f):
         current += 1
     return z
 
+#Takes counted input and number of sentences requested as input and returns the final summarized sentences as output
+def clustering(cin, numCentroids):
+    minNoun, minVerb, maxNoun, maxVerb = cin[0][0], cin[0][1], cin[0][0], cin[0][1]
+    for a in cin:
+        if(a[0]<minNoun):
+            minNoun = a[0]
+        elif(a[0]>maxNoun):
+            maxNoun = a[0]
+        if(a[1]<minVerb):
+            minVerb = a[1]
+        elif(a[1]>maxVerb):
+            maxVerb = a[1]
+    #print("minNoun = " + str(minNoun) + " minVerb " + str(minVerb) + " maxNoun " + str(maxNoun) + " maxVerb " + str(maxVerb))
+    centroidPosition = []
+    count = 0
+    #Initialize centroid position to be within bounds of numNouns and numVerbs
+    for i in range(numCentroids):
+        centroidPosition.append([])
+        centroidPosition[count].append(randint(minNoun, maxNoun))
+        centroidPosition[count].append(randint(minVerb, maxVerb))
+        count += 1
+    #Repeats k-means 10 times
+    #for i in range(10):
+    #    print(minNoun)
+
 #MAIN PROGRAM STARTS HERE
 
 #Open text file
 with open(input("Enter a filename: "), 'r') as f:
     file = f.read()
 
+percent = int(input("How much summarization? "))
+
 #Tokenize input into sentences
 tokenizedInput = tokenize(file)
 print("Finished tokenization")
+
+numSentences = int(len(tokenizedInput)*(percent/100))
+
 countedInput = counting(tokenizedInput)
 print("Finished counting")
 
 for a in countedInput:
     print(a)
+
+clustering(countedInput, numSentences)
